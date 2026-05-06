2e25d52e0387ba3bd610ecaa8cab3297d95a3fbe
Matthew Hiles <matthew.hiles@gmail.com>
2026-05-06 13:03:28 -0400

    improve ADC use with battery

--- battery.go
+++ battery.go
@@ -2,32 +2,40 @@ package cardputer
 
 import "machine"
 
+const (
+	batteryADCResolutionBits = 12
+	batteryADCMax            = (1 << batteryADCResolutionBits) - 1
+	batteryADCReferenceMV    = 3300
+	batteryDividerNumerator  = 2
+	batteryEmptyMV           = 3300
+	batteryFullMV            = 4200
+)
+
 // BatteryADC exposes the ADC channel used to sample the battery divider.
 // It is configured during package initialization.
 var BatteryADC = machine.ADC{Pin: BatterySense}
 
 func init() {
-	// TODO: Figure out what settings actually work best here
 	BatterySense.Configure(machine.PinConfig{Mode: machine.PinAnalog})
-	BatteryADC.Configure(machine.ADCConfig{Resolution: 8, Samples: 32})
+	BatteryADC.Configure(machine.ADCConfig{Resolution: batteryADCResolutionBits, Samples: 32})
+}
+
+// BatteryMilliVolts returns the measured battery voltage in millivolts.
+// This is derived from the ADC reading and the board's 2:1 divider.
+func BatteryMilliVolts() int {
+	raw := int(BatteryADC.Get())
+	return raw * batteryADCReferenceMV * batteryDividerNumerator / batteryADCMax
 }
 
 // BatteryLevel returns an approximate battery percentage in the range 0..100.
 func BatteryLevel() int {
-	// As I understand the docs for the cardputer, the battery voltage is passed to a voltage divider
-	// that divides voltage in half. If we expect the fully charged battery to be at 3.7 volts and our ref
-	// voltage is 3.3 volts, than our ADC (in 8 bit resolution) will read ~144 at full charge.
-	// (3.7/2) / 3.3 * 246 ≈ 144
-	// I've rounded down to 143 for the pleasure of actually seeing a 100% charged battery
-	// TODO: after confirming this works, add a max() in there so we don't show MORE than 100%
-	// TODO: make this measurement linear if it isn't; that is, if the battery follows a logarthmic
-	// discharge, correct that to be linear.
-	level := int(BatteryADC.Get()) * 100 / 144
-	if level > 100 {
-		return 100
-	}
-	if level < 0 {
+	mv := BatteryMilliVolts()
+	switch {
+	case mv <= batteryEmptyMV:
 		return 0
+	case mv >= batteryFullMV:
+		return 100
+	default:
+		return (mv - batteryEmptyMV) * 100 / (batteryFullMV - batteryEmptyMV)
 	}
-	return level
 }
